ff0d39c18c7564fe02f5d66f7d35372fd0d21113
UlfetKacay <ukacay87@gmail.com>
2021-09-30 18:50:30 +0300

    firebase updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,19 @@ import "firebase/auth";
 import "firebase/database";
 import { successToastify } from "./customToastify";
 
-const firebaseApp = firebase.initializeApp({
+const devConfig  = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-});
+};
+const prodConfig = {};
+
+const firebaseConfig = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
+
+firebase.initializeApp(firebaseConfig);
 
 export const createUser = async (email, password, username) => {
   try {
@@ -80,4 +85,4 @@ export const forgotPassword = (email) => {
   alert("Please check your mail box!");
 };
 
-export default firebaseApp;
+export default firebase;
